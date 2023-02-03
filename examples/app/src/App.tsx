45c841f8e5b283d15a00735ff7d383d9fb6b45ed
Victor Jimenez <betabandido@gmail.com>
2023-02-03 16:16:44 +0100

    Broken

---
+++ examples/app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MyComponent } from "@vp/another-package";
+
+function App() {
+  return (
+    <div>
+      Hello!
+        <MyComponent />
+    </div>
+  );
+}
+
+export default App;
